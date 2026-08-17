122fab1b837eb9d7eb55425948d140adde58a606
Shusrita Venugopal <63789652+shusritavenugopal@users.noreply.github.com>
2026-08-17 16:58:18 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> sfreq = new HashMap<>();
+        Map<Character, Integer> tfreq = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sfreq.put(s.charAt(i), sfreq.getOrDefault(s.charAt(i), 0) + 1);
+            tfreq.put(t.charAt(i), tfreq.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return sfreq.equals(tfreq);
+    }
+}
